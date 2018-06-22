42b475d599882afa7a9f41bea64e97118b8e06b2
Freddy-Lazo <freddy.lazo@pucp.pe>
2018-06-21 19:31:41 -0500

    se puede ver los videos en youtube =D

--- app/src/main/java/freddy/mymovieapp/adapter/VideoAdapter.java
+++ app/src/main/java/freddy/mymovieapp/adapter/VideoAdapter.java
@@ -1,6 +1,8 @@
 package freddy.mymovieapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +16,10 @@ import freddy.mymovieapp.model.Video;
 public class VideoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Video> trailerData;
+    private  Context ctx;
 
     public VideoAdapter(Context ctx, List<Video> trailerData) {
-       // this.ctx = ctx;
+        this.ctx = ctx;
         this.trailerData = trailerData;
     }
 
@@ -28,9 +31,19 @@ public class VideoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder genericHolder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder genericHolder, final int position) {
         final MovieViewHolderHelper.VideoViewHolder holder = (MovieViewHolderHelper.VideoViewHolder) genericHolder;
         holder.movieTrailerName.setText(trailerData.get(position).getName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String videoKey = trailerData.get(position).getKey();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v="+videoKey));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("VIDEO_ID", videoKey);
+                ctx.startActivity(intent);
+            }
+        });
     }
 
     @Override
